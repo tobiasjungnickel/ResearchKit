092f1950c43a21b41dfcbc285fd420875c422b8c
Tobias Jungnickel <tobias.jungnickel@googlemail.com>
2017-01-30 15:50:02 +0100

    adding left and right items into separate subview to better adjust them

--- ResearchKit/ActiveTasks/ORKImplicitAssociationContentView.m
+++ ResearchKit/ActiveTasks/ORKImplicitAssociationContentView.m
@@ -54,13 +54,18 @@
 
 
 @implementation ORKImplicitAssociationContentView {
+    
+    UIView *_leftItemContainer;
+    UIView *_rightItemContainer;
     UIView *_buttonContainer;
+    
     ORKSubheadlineLabel *_leftItemLabel1;
     ORKSubheadlineLabel *_rightItemLabel1;
     ORKSubheadlineLabel *_leftItemLabel2;
     ORKSubheadlineLabel *_rightItemLabel2;
     ORKSubheadlineLabel *_leftDividerLabel;
     ORKSubheadlineLabel *_rightDividerLabel;
+    
     ORKHeadlineLabel *_wrongLabel;
     
     NSNumberFormatter *_formatter;
@@ -70,33 +75,45 @@
     self = [super init];
     if (self) {
         
-        _termLabel = [ORKHeadlineLabel new];
-        _termLabel.textAlignment = NSTextAlignmentCenter;
-        _termLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        _leftItemContainer = [UIView new];
+        _leftItemContainer.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        _rightItemContainer = [UIView new];
+        _rightItemContainer.translatesAutoresizingMaskIntoConstraints = NO;
         
         _leftItemLabel1 = [ORKSubheadlineLabel new];
+        _leftItemLabel1.textColor = [UIColor blueColor];
         _leftItemLabel1.textAlignment = NSTextAlignmentCenter;
         _leftItemLabel1.translatesAutoresizingMaskIntoConstraints = NO;
         
-        _rightItemLabel1 = [ORKSubheadlineLabel new];
-        _rightItemLabel1.textAlignment = NSTextAlignmentCenter;
-        _rightItemLabel1.translatesAutoresizingMaskIntoConstraints = NO;
-        
-        _leftItemLabel2 = [ORKSubheadlineLabel new];
-        _leftItemLabel2.textAlignment = NSTextAlignmentCenter;
-        _leftItemLabel2.translatesAutoresizingMaskIntoConstraints = NO;
-        
-        _rightItemLabel2 = [ORKSubheadlineLabel new];
-        _rightItemLabel2.textAlignment = NSTextAlignmentCenter;
-        _rightItemLabel2.translatesAutoresizingMaskIntoConstraints = NO;
-        
         _leftDividerLabel = [ORKSubheadlineLabel new];
+        _leftDividerLabel.textColor = [UIColor blackColor];
         _leftDividerLabel.textAlignment = NSTextAlignmentCenter;
         _leftDividerLabel.translatesAutoresizingMaskIntoConstraints = NO;
         
+        _leftItemLabel2 = [ORKSubheadlineLabel new];
+        _leftItemLabel2.textColor = [UIColor colorWithRed:45.0/255.0 green:145.0/255.0 blue:0.0 alpha:1.0];
+        _leftItemLabel2.textAlignment = NSTextAlignmentCenter;
+        _leftItemLabel2.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        _rightItemLabel1 = [ORKSubheadlineLabel new];
+        _rightItemLabel1.textColor = [UIColor blueColor];
+        _rightItemLabel1.textAlignment = NSTextAlignmentCenter;
+        _rightItemLabel1.translatesAutoresizingMaskIntoConstraints = NO;
+        
         _rightDividerLabel = [ORKSubheadlineLabel new];
+        _rightDividerLabel.textColor = [UIColor blackColor];
         _rightDividerLabel.textAlignment = NSTextAlignmentCenter;
         _rightDividerLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        _rightItemLabel2 = [ORKSubheadlineLabel new];
+        _rightItemLabel2.textColor = [UIColor colorWithRed:45.0/255.0 green:145.0/255.0 blue:0.0 alpha:1.0];
+        _rightItemLabel2.textAlignment = NSTextAlignmentCenter;
+        _rightItemLabel2.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        _termLabel = [ORKHeadlineLabel new];
+        _termLabel.textAlignment = NSTextAlignmentCenter;
+        _termLabel.translatesAutoresizingMaskIntoConstraints = NO;
         
         _wrongLabel = [ORKHeadlineLabel new];
         _wrongLabel.textAlignment = NSTextAlignmentCenter;
@@ -115,28 +132,32 @@
         
         _lastTappedButton = -1;
         
+        [self addSubview:_leftItemContainer];
+        [self addSubview:_rightItemContainer];
         [self addSubview:_termLabel];
-        [self addSubview:_leftItemLabel1];
-        [self addSubview:_rightItemLabel1];
-        [self addSubview:_leftItemLabel2];
-        [self addSubview:_rightItemLabel2];
-        [self addSubview:_leftDividerLabel];
-        [self addSubview:_rightDividerLabel];
         [self addSubview:_wrongLabel];
         [self addSubview:_buttonContainer];
         
         [_buttonContainer addSubview:_tapButton1];
         [_buttonContainer addSubview:_tapButton2];
         
+        [_leftItemContainer addSubview:_leftItemLabel1];
+        [_leftItemContainer addSubview:_leftDividerLabel];
+        [_leftItemContainer addSubview:_leftItemLabel2];
+        
+        [_rightItemContainer addSubview:_rightItemLabel1];
+        [_rightItemContainer addSubview:_rightDividerLabel];
+        [_rightItemContainer addSubview:_rightItemLabel2];
+        
         self.translatesAutoresizingMaskIntoConstraints = NO;
         
-        _termLabel.text = ORKLocalizedString(@"TERM_LABEL", nil);
         _leftItemLabel1.text = @"LEFT_1";
+        _leftDividerLabel.text = @"LEFT_DIVIDER";
         _rightItemLabel1.text = @"RIGHT_1";
         _leftItemLabel2.text = @"LEFT_2";
-        _rightItemLabel2.text = @"RIGHT_2";
-        _leftDividerLabel.text = @"LEFT_DIVIDER";
         _rightDividerLabel.text = @"RIGHT_DIVIDER";
+        _rightItemLabel2.text = @"RIGHT_2";
+        _termLabel.text = ORKLocalizedString(@"TERM_LABEL", nil);
         _wrongLabel.text = @"X";
         
         [self setUpConstraints];
@@ -145,7 +166,9 @@
         self.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:0.5];
         self.termLabel.backgroundColor = [UIColor orangeColor];
         _wrongLabel.backgroundColor = [UIColor greenColor];
-        _buttonContainer.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.25];
+        _buttonContainer.backgroundColor = [UIColor blueColor];
+        _leftItemContainer.backgroundColor = [UIColor redColor];
+        _rightItemContainer.backgroundColor = [UIColor purpleColor];
 #endif
     }
     return self;
@@ -170,7 +193,91 @@
 - (void)setUpConstraints {
     NSMutableArray *constraints = [NSMutableArray array];
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(_buttonContainer, _termLabel, _leftItemLabel1, _rightItemLabel1, _leftItemLabel2, _rightItemLabel2, _leftDividerLabel, _rightDividerLabel, _wrongLabel, _tapButton1, _tapButton2);
+    NSDictionary *views = NSDictionaryOfVariableBindings(_leftItemContainer, _leftItemLabel1, _leftDividerLabel, _leftItemLabel2, _rightItemContainer, _rightItemLabel1, _rightDividerLabel, _rightItemLabel2, _termLabel, _wrongLabel, _buttonContainer, _tapButton1, _tapButton2);
+    
+    // left items
+    
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:_leftItemContainer
+                                                        attribute:NSLayoutAttributeTop
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:self
+                                                        attribute:NSLayoutAttributeTop
+                                                       multiplier:1.0
+                                                         constant:8.0]];
+    
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:_leftItemContainer
+                                                        attribute:NSLayoutAttributeLeft
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:self
+                                                        attribute:NSLayoutAttributeLeft
+                                                       multiplier:1.0
+                                                         constant:8.0]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_leftItemLabel1]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_leftDividerLabel]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_leftItemLabel2]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_leftItemLabel1]-(>=10)-[_leftDividerLabel]-(>=10)-[_leftItemLabel2]|"
+                                             options:NSLayoutFormatAlignAllCenterX
+                                             metrics:nil
+                                               views:views]];
+    
+    // right items
+    
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:_rightItemContainer
+                                                        attribute:NSLayoutAttributeTop
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:self
+                                                        attribute:NSLayoutAttributeTop
+                                                       multiplier:1.0
+                                                         constant:8.0]];
+    
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:self
+                                                        attribute:NSLayoutAttributeRight
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:_rightItemContainer
+                                                        attribute:NSLayoutAttributeRight
+                                                       multiplier:1.0
+                                                         constant:8.0]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_rightItemLabel1]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_rightDividerLabel]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_rightItemLabel2]|"
+                                             options:(NSLayoutFormatOptions)0
+                                             metrics:nil
+                                               views:views]];
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_rightItemLabel1]-(>=10)-[_rightDividerLabel]-(>=10)-[_rightItemLabel2]|"
+                                             options:NSLayoutFormatAlignAllCenterX
+                                             metrics:nil
+                                               views:views]];
+    
+    // terms
     
     [constraints addObject:[NSLayoutConstraint constraintWithItem:_termLabel
                                                         attribute:NSLayoutAttributeCenterX
@@ -188,47 +295,7 @@
                                                        multiplier:1.0
                                                          constant:0.0]];
     
-    
-    [constraints addObject:[NSLayoutConstraint constraintWithItem:_leftItemLabel1
-                                                        attribute:NSLayoutAttributeTop
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:self
-                                                        attribute:NSLayoutAttributeTop
-                                                       multiplier:1.0
-                                                         constant:0.0]];
-    
-    [constraints addObject:[NSLayoutConstraint constraintWithItem:_leftItemLabel1
-                                                        attribute:NSLayoutAttributeLeft
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:self
-                                                        attribute:NSLayoutAttributeLeft
-                                                       multiplier:1.0
-                                                         constant:10.0]];
-    
-    [constraints addObject:[NSLayoutConstraint constraintWithItem:_rightItemLabel1
-                                                        attribute:NSLayoutAttributeTop
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:self
-                                                        attribute:NSLayoutAttributeTop
-                                                       multiplier:1.0
-                                                         constant:0.0]];
-    
-    [constraints addObject:[NSLayoutConstraint constraintWithItem:_rightItemLabel1
-                                                        attribute:NSLayoutAttributeRight
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:self
-                                                        attribute:NSLayoutAttributeRight
-                                                       multiplier:1.0
-                                                         constant:-10.0]];
-    
-    [constraints addObject: [NSLayoutConstraint constraintWithItem:_wrongLabel
-                                                         attribute:NSLayoutAttributeFirstBaseline
-                                                         relatedBy:NSLayoutRelationEqual
-                                                            toItem:_termLabel
-                                                         attribute:NSLayoutAttributeFirstBaseline
-                                                        multiplier:1.0
-                                                          constant:70.0]];
-    
+    // buttons
     
     [constraints addObject: [NSLayoutConstraint constraintWithItem:self
                                                          attribute:NSLayoutAttributeBottom
@@ -237,39 +304,6 @@
                                                          attribute:NSLayoutAttributeBottom
                                                         multiplier:1.0
                                                           constant:36.0]];
-    
-    
-    [constraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_leftItemLabel1]-(>=10)-[_leftDividerLabel]-(>=10)-[_leftItemLabel2]"
-                                             options:NSLayoutFormatAlignAllCenterX
-                                             metrics:nil
-                                               views:views]];
-    
-    [constraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_rightItemLabel1]-(>=10)-[_rightDividerLabel]-(>=10)-[_rightItemLabel2]"
-                                             options:NSLayoutFormatAlignAllCenterX
-                                             metrics:nil
-                                               views:views]];
-    
-    
-    [constraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_termLabel]-(>=10)-[_buttonContainer]"
-                                             options:NSLayoutFormatAlignAllCenterX
-                                             metrics:nil
-                                               views:views]];
-
-    [constraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_termLabel]-|"
-                                             options:(NSLayoutFormatOptions)0
-                                             metrics:nil
-                                               views:views]];
-    
-    [constraints addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_wrongLabel]-|"
-                                             options:(NSLayoutFormatOptions)0
-                                             metrics:nil
-                                               views:views]];
-    
     
     [constraints addObjectsFromArray:
      [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_tapButton1]|"
@@ -295,6 +329,14 @@
                                                         attribute:NSLayoutAttributeCenterY
                                                        multiplier:1.0
                                                          constant:0.0]];
+    
+    // terms and buttons alignment
+    
+    [constraints addObjectsFromArray:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_termLabel]-(10)-[_wrongLabel]-(>=10)-[_buttonContainer]"
+                                             options:NSLayoutFormatAlignAllCenterX
+                                             metrics:nil
+                                               views:views]];
 
     [NSLayoutConstraint activateConstraints:constraints];
 }
