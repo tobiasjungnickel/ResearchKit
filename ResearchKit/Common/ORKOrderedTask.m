83ce43bea32fb2a42135fecd1fac0661ba173246
Tobias Jungnickel <tobias.jungnickel@googlemail.com>
2017-02-08 09:56:52 +0100

    setting stepDuration on interim to fill view

--- ResearchKit/Common/ORKOrderedTask.m
+++ ResearchKit/Common/ORKOrderedTask.m
@@ -2053,6 +2053,7 @@ void ORKStepArrayAddStep(NSMutableArray *array, ORKStep *step) {
                 [trials addObject:iaTrial];
             }
             step.trials = trials;
+            step.stepDuration = 1;
             //step.recorderConfigurations = recorderConfigurations;
             ORKStepArrayAddStep(steps, step);
         }
@@ -2083,6 +2084,7 @@ void ORKStepArrayAddStep(NSMutableArray *array, ORKStep *step) {
         }
             
         step.trials = trials;
+        step.stepDuration = 1;
         //step.recorderConfigurations = recorderConfigurations;
             
         ORKStepArrayAddStep(steps, step);
@@ -2119,6 +2121,7 @@ void ORKStepArrayAddStep(NSMutableArray *array, ORKStep *step) {
         }
         
         step.trials = trials;
+        step.stepDuration = 1;
         //step.recorderConfigurations = recorderConfigurations;
         
         ORKStepArrayAddStep(steps, step);
